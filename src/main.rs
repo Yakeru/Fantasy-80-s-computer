0a683a411df742b11f0e7e2dec492b5469b779d2
Yakeru <yakeru@gmail.com>
2022-04-29 10:20:52 -0400

    CRT effect
    test of a home made CRT effect

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ use std::time::{
     Instant
 };
 
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 240;
+const WIDTH: u32 = 1280;
+const HEIGHT: u32 = 960;
 
 fn main()-> Result<(), Error> 
 {
@@ -29,6 +29,10 @@ fn main()-> Result<(), Error>
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
         Pixels::new(WIDTH, HEIGHT, surface_texture)?
     };
+
+    let mut last_refresh: Instant = Instant::now();
+    let mut sec_counter: Instant = Instant::now();
+    let mut apply_effect: bool = false;
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
@@ -49,54 +53,116 @@ fn main()-> Result<(), Error>
                 // applications which do not always need to. Applications that redraw continuously
                 // can just render here instead.
                 let frame = pixels.get_frame();
-                draw(frame);
+
+                if last_refresh.elapsed().as_millis() >= 33
+                {
+                    draw(frame, apply_effect);
+                    last_refresh = Instant::now();
+                }
                 pixels.render().expect("Pixels render oups");
                 window.request_redraw(); 
+
+                
             }
             _ => ()
+        }
+        if sec_counter.elapsed().as_millis() >= 1000
+        {
+            apply_effect = !apply_effect;
+            sec_counter = Instant::now();
         }
     });
 }
 
-fn draw(frame_buffer: &mut[u8])
+fn draw(frame_buffer: &mut[u8], apply_effect: bool)
 {
     let last_refresh: Instant = Instant::now();
 
     let mut random = rand::thread_rng();
-    let chunk_size: u32 = WIDTH * (HEIGHT/24);
-    let mut pixel_count: u32 = 0;
+    let chunk_size: u32 = WIDTH * (HEIGHT/48);
+    
     let mut r: u8 = random.gen_range(0..255);
     let mut g: u8 = random.gen_range(0..255);
     let mut b: u8 = random.gen_range(0..255);
+    
+    let mut pixel_count: u32 = 0;
     let mut line_count: u32 = 0;
+    let mut sub_pixel_count = 0;
 
     for pixel in frame_buffer.chunks_exact_mut(4) 
     {
-        pixel[0] = r;
-        pixel[1] = g;
-        pixel[2] = b;
-        pixel[3] = 255;
-
         if pixel_count % chunk_size == 0 
         {
             r = random.gen_range(0..255);
             g = random.gen_range(0..255);
             b = random.gen_range(0..255);
-        } 
-        
-        if line_count % 2 == 0
-        {
-            pixel[0] = pixel[0]/2;
-            pixel[1] = pixel[1]/2;
-            pixel[2] = pixel[2]/2;
         }
 
-        pixel_count = pixel_count + 1;
+        pixel[0] = r;
+        pixel[1] = g;
+        pixel[2] = b;
+        pixel[3] = 255;
+
+        if apply_effect
+        {
+            crt_effect(pixel, sub_pixel_count, line_count)
+        }
+
+        pixel_count += 1;
+        sub_pixel_count = if sub_pixel_count == 2 { 0 } else { sub_pixel_count + 1 };
 
         if pixel_count % WIDTH == 0
         {
-            line_count = line_count + 1
+            line_count += 1;
+            sub_pixel_count = 0;
         }
     }
+
     println!("draw time {}ms", last_refresh.elapsed().as_millis());
+}
+
+fn crt_effect(pixel: &mut[u8], sub_pixel_count: u32, line_count: u32) 
+{
+    //CRT effect
+    if sub_pixel_count == 0
+    {
+        pixel[1] = 0;
+        pixel[2] = 0;
+    }
+
+    if sub_pixel_count == 1
+    {
+        pixel[0] = 0;
+        pixel[2] = 0;
+    }
+
+    if sub_pixel_count == 2
+    {
+        pixel[0] = 0;
+        pixel[1] = 0;
+    }
+    
+    if line_count % 4 == 0
+    {
+        if sub_pixel_count == 0
+        {
+            pixel[0] = if pixel[0] <= 20 {0} else {pixel[0]-20};
+            pixel[1] = 0;
+            pixel[2] = 0;
+        }
+
+        if sub_pixel_count == 1
+        {
+            pixel[0] = 0;
+            pixel[1] = if pixel[1] <= 20 {0} else {pixel[1]-20};
+            pixel[2] = 0;
+        }
+
+        if sub_pixel_count == 2
+        {
+            pixel[0] = 0;
+            pixel[1] = 0;
+            pixel[2] = if pixel[2] <= 20 {0} else {pixel[2]-20};
+        }
+    }
 }
