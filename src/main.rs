37059df1d9020d2dfd3d743cd3affcf16b39877e
Yakeru <yakeru@gmail.com>
2022-05-03 14:09:58 -0400

    Integer scaling and key presses
    Functionning integer scaling
    Key presses

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use winit::{
-    event::{Event, WindowEvent},
+    event::{Event, WindowEvent, VirtualKeyCode},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
-    dpi::PhysicalSize,
+    dpi::PhysicalSize
 };
-
+use winit_input_helper::WinitInputHelper;
 use pixels::{Error, Pixels, SurfaceTexture};
 use rand::Rng;
 use std::time::{
     Instant
 };
+
+ 
 
 const WIDTH: u32 = 1280;
 const HEIGHT: u32 = 960;
@@ -22,7 +24,15 @@ const FPS: u128 = 16; //ms per frame, so 16 = 60fps, 32 = 30fps, 1000 = 1fps
 const BORDER: u32 = 100;
 const BKG_COLOR: (u8, u8, u8) = (0, 0, 254);
 
+
+
 fn main()-> Result<(), Error> {
+
+    let mut color_map: [(u8, u8, u8); 32];
+
+    let mut horizontal_multiplier: usize = 3;
+    let mut vertical_multiplier: usize = 4;
+    let mut crt_effet_on: bool = false;
 
     let event_loop = EventLoop::new();
     let builder = WindowBuilder::new()
@@ -32,6 +42,8 @@ fn main()-> Result<(), Error> {
         .with_resizable(false);
 
     let window = builder.build(&event_loop).expect("Window creation failed !");
+
+    let mut input = WinitInputHelper::new();
     
     let mut pixels = {
         let window_size = window.inner_size();
@@ -40,7 +52,7 @@ fn main()-> Result<(), Error> {
     };
 
     let mut virtual_frame_buffer: VirtualFrameBuffer = VirtualFrameBuffer::new(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
-    draw_a_thing(virtual_frame_buffer.get_frame());
+    draw_test_grid(virtual_frame_buffer.get_frame());
 
     let mut last_refresh: Instant = Instant::now();
 
@@ -60,12 +72,14 @@ fn main()-> Result<(), Error> {
                 // Queue a RedrawRequested event.
                 if last_refresh.elapsed().as_millis() >= FPS {
                     let render_time: Instant = Instant::now();
-
-                    render_virtualfb_to_pixelfb(virtual_frame_buffer.get_frame(), pixels.get_frame());
-
+                    clear_frame_buffer(pixels.get_frame());
+                    upscale_virtualfb_to_pixelfb(virtual_frame_buffer.get_frame(), pixels.get_frame(), horizontal_multiplier, vertical_multiplier);
+                    
+                    if crt_effet_on {
+                        apply_crt_effect(pixels.get_frame());
+                    }
                     //draw_loading_border(pixels.get_frame());
                     //draw_background(pixels.get_frame());
-                    //apply_crt_effect(pixels.get_frame());
                     
                     println!("draw time {}us", render_time.elapsed().as_micros());
                     pixels.render().expect("Pixels render oups");
@@ -75,7 +89,59 @@ fn main()-> Result<(), Error> {
             }
             _ => ()
         }
+
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                println!("The Escape key was pressed; stopping");
+                return;
+            }
+
+            if input.key_pressed(VirtualKeyCode::Right) {
+                horizontal_multiplier += 1;
+                if horizontal_multiplier > 3 {
+                    horizontal_multiplier = 3
+                }
+                return;
+            }
+
+            if input.key_pressed(VirtualKeyCode::Left) {
+                horizontal_multiplier -= 1;
+                if horizontal_multiplier < 1 {
+                    horizontal_multiplier = 1
+                }
+                return;
+            }
+
+            if input.key_pressed(VirtualKeyCode::Down) {
+                vertical_multiplier += 1;
+                if vertical_multiplier > 4 {
+                    vertical_multiplier = 4
+                }
+                return;
+            }
+
+            if input.key_pressed(VirtualKeyCode::Up) {
+                vertical_multiplier -= 1;
+                if vertical_multiplier < 1 {
+                    vertical_multiplier = 1
+                }
+                return;
+            }
+
+            if input.key_pressed(VirtualKeyCode::Return) {
+                crt_effet_on = !crt_effet_on;
+                return;
+            }
+        }
     });
+}
+
+fn clear_frame_buffer(frame_buffer: &mut[u8]) {
+    for value in frame_buffer.chunks_exact_mut(1) {
+        value[0] = 0;
+    }
 }
 
 // fn draw_loading_border(frame_buffer: &mut[u8]) {
@@ -134,61 +200,61 @@ fn main()-> Result<(), Error> {
 //     }
 // }
 
-// fn apply_crt_effect(frame_buffer: &mut[u8]) {
+fn apply_crt_effect(frame_buffer: &mut[u8]) {
 
-//     let mut screen_pixel_count: u32 = 0;
-//     let mut line_count: u32 = 0;
-//     let mut sub_pixel_count: u32 = 0;
+    let mut screen_pixel_count: u32 = 0;
+    let mut line_count: u32 = 0;
+    let mut sub_pixel_count: u32 = 0;
 
-//     for pixel in frame_buffer.chunks_exact_mut(4) {
+    for pixel in frame_buffer.chunks_exact_mut(4) {
 
-//         match sub_pixel_count {
-//             0 => {
-//                 pixel[1] = 0;
-//                 pixel[2] = 0;
-//             },
-//             1 => {
-//                 pixel[0] = 0;
-//                 pixel[2] = 0;
-//             },
-//             2 => {
-//                 pixel[0] = 0;
-//                 pixel[1] = 0;
-//             },
-//             3_u32..=u32::MAX => {}
-//         }
+        match sub_pixel_count {
+            0 => {
+                pixel[1] = 0;
+                pixel[2] = 0;
+            },
+            1 => {
+                pixel[0] = 0;
+                pixel[2] = 0;
+            },
+            2 => {
+                pixel[0] = 0;
+                pixel[1] = 0;
+            },
+            3_u32..=u32::MAX => {}
+        }
         
-//         if line_count % 4 == 0 {
-//             match sub_pixel_count {
-//                 0 => {
-//                     pixel[0] = if pixel[0] <= 20 {0} else {pixel[0] - 20};
-//                     pixel[1] = 0;
-//                     pixel[2] = 0;
-//                 },
-//                 1 => {
-//                     pixel[0] = 0;
-//                     pixel[1] = if pixel[1] <= 20 {0} else {pixel[1] - 20};
-//                     pixel[2] = 0;
-//                 },
-//                 2 => {
-//                     pixel[0] = 0;
-//                     pixel[1] = 0;
-//                     pixel[2] = if pixel[2] <= 20 {0} else {pixel[2] - 20};
-//                 },
-//                 3_u32..=u32::MAX => {}
-//             }
-//         }
+        if line_count % 4 == 0 {
+            match sub_pixel_count {
+                0 => {
+                    pixel[0] = if pixel[0] <= 20 {0} else {pixel[0] - 20};
+                    pixel[1] = 0;
+                    pixel[2] = 0;
+                },
+                1 => {
+                    pixel[0] = 0;
+                    pixel[1] = if pixel[1] <= 20 {0} else {pixel[1] - 20};
+                    pixel[2] = 0;
+                },
+                2 => {
+                    pixel[0] = 0;
+                    pixel[1] = 0;
+                    pixel[2] = if pixel[2] <= 20 {0} else {pixel[2] - 20};
+                },
+                3_u32..=u32::MAX => {}
+            }
+        }
 
-//         screen_pixel_count += 1;
-//         sub_pixel_count = if sub_pixel_count == 2 {0} else {sub_pixel_count + 1};
+        screen_pixel_count += 1;
+        sub_pixel_count = if sub_pixel_count == 2 {0} else {sub_pixel_count + 1};
 
-//         if screen_pixel_count == WIDTH {
-//             line_count += 1;
-//             screen_pixel_count = 0;
-//             sub_pixel_count = 0;
-//         }
-//     }
-// }
+        if screen_pixel_count == WIDTH {
+            line_count += 1;
+            screen_pixel_count = 0;
+            sub_pixel_count = 0;
+        }
+    }
+}
 
 // struct Sprite {
 //     pos_x: u32,
@@ -248,7 +314,7 @@ impl VirtualFrameBuffer {
     }
 }
 
-fn draw_a_thing(virtual_fb: &mut[u8]) {
+fn draw_test_grid(virtual_fb: &mut[u8]) {
     let mut screen_pixel_count: u32 = 0;
     let mut line_count: u32 = 0;
 
@@ -299,20 +365,17 @@ fn draw_a_thing(virtual_fb: &mut[u8]) {
     }
 }
 
-fn render_virtualfb_to_pixelfb(virtual_fb: &mut[u8], pixels_frame: &mut[u8]) {
+fn upscale_virtualfb_to_pixelfb(virtual_fb: &mut[u8], pixels_frame: &mut[u8], integer_width_multiplier: usize, integer_height_multiplier: usize) {
     
-    let mut virt_total_pixel_counter: usize = 0;
-    let mut virt_pixel_counter: usize = 0;
+    let mut virt_line_pixel_counter: usize = 0;
     let mut virt_line_counter: usize = 0;
+    let pixels_sub_pixel_count = 4;
 
-    const NB_OF_LINES_TO_SKIP: usize = 4;
-    const NB_OF_PIXELS_TO_SKIP: usize = 4;
-    
-    while virt_total_pixel_counter < VIRTUAL_WIDTH as usize * VIRTUAL_HEIGHT as usize{
+    for pixel in virtual_fb {
 
-        let color_index = virtual_fb[virt_total_pixel_counter];
+        //Temporary color index to RGB mapping
         let mut rgb: (u8, u8, u8) = (0, 0, 0);
-        match color_index {
+        match pixel {
             0 => {
                 rgb.0 = 0;
                 rgb.1 = 0;
@@ -340,30 +403,26 @@ fn render_virtualfb_to_pixelfb(virtual_fb: &mut[u8], pixels_frame: &mut[u8]) {
             }
         }
 
-        for identical_line in 0..4 {
-            let line_offset: usize = identical_line * NB_OF_LINES_TO_SKIP * WIDTH as usize;
-            let offset: usize = 12 * virt_pixel_counter + WIDTH as usize * virt_line_counter * NB_OF_PIXELS_TO_SKIP * NB_OF_LINES_TO_SKIP + line_offset;
-            pixels_frame[0 + offset] = rgb.0;
-            pixels_frame[1 + offset] = 0;
-            pixels_frame[2 + offset] = 0;
-            pixels_frame[3 + offset] = 254;
-            pixels_frame[4 + offset] = 0;
-            pixels_frame[5 + offset] = rgb.1;
-            pixels_frame[6 + offset] = 0;
-            pixels_frame[7 + offset] = 254;
-            pixels_frame[8 + offset] = 0;
-            pixels_frame[9 + offset] = 0;
-            pixels_frame[10 + offset] = rgb.2;
-            pixels_frame[11 + offset] = 254;
+        //Offset between virtual frame buffer and pixel's frame buffer
+        //if scaling is applied, it represents the offset between virtual frame buffer's pixel and
+        //pixel's top-left corner of scalled pixel
+        let global_offset = pixels_sub_pixel_count * virt_line_pixel_counter * integer_width_multiplier 
+        + WIDTH as usize * pixels_sub_pixel_count * virt_line_counter * integer_height_multiplier;
+        
+        for horizontal_copy in 0..integer_width_multiplier {
+            for vertical_copy in 0..integer_height_multiplier {
+                let scaling_offset:usize = pixels_sub_pixel_count * horizontal_copy + WIDTH as usize * pixels_sub_pixel_count * vertical_copy;
+                pixels_frame[0 + global_offset + scaling_offset] = rgb.0;
+                pixels_frame[1 + global_offset + scaling_offset] = rgb.1;
+                pixels_frame[2 + global_offset + scaling_offset] = rgb.2;
+                pixels_frame[3 + global_offset + scaling_offset] = 254;
+            }
         }
 
-        virt_pixel_counter += 1;
-        if virt_pixel_counter == VIRTUAL_WIDTH as usize {
-            virt_pixel_counter = 0;
+        virt_line_pixel_counter += 1;
+        if virt_line_pixel_counter == VIRTUAL_WIDTH as usize {
+            virt_line_pixel_counter = 0;
             virt_line_counter += 1;
         }
-
-        virt_total_pixel_counter += 1;
     }
-    
 }
